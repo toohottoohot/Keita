f32b028c237a205655c8000cea41b4957564e42e
LAPTOP-E8GKPCBA\k_ten <makyukyuk@gmail.com>
2017-06-15 13:01:31 +0900

    aaa

--- homework.php
+++ homework.php
@@ -3,7 +3,7 @@
   //文字列連結を用いて、自分の名前をブラウザ上に表示する。
   $first_name = 'Keita';
   $last_name = 'Tanikawa';
-echo $first_name,$last_name;
+echo $first_name.$last_name;
   //練習問題2
   //以下の計算をそれぞれ行い、ブラウザ上に表示する。
   //8 * 3の計算
@@ -46,6 +46,9 @@ echo $var %= 3;
   //練習問題6
   //以下の$a, $b, $cは三角形の辺の長さである。
   //この3辺で三角形は成立可能か($a + $b > $c)を求めよ。もし、三角形が成立可能ならばブラウザに「可能」、成立不可能ならば「不可能」と表示せよ。
+$a = 3;
+$b = 4;
+$c = 5;
 if($a + $b > $c){
   echo '可能';
 }
@@ -63,7 +66,26 @@ else{
     2   ->   D
     1   ->   E
   */
-
+  $score = 1;
+   switch($score){
+     case 5:
+     echo'A';
+      break;
+      case 4:
+     echo 'B';
+      break;
+      case 3:
+     echo 'C';
+      break;
+      case 2:
+     echo 'D';
+      break;
+      case 1:
+     echo 'E';
+      break;
+     default:
+      break;
+   }
 
   //練習問題8
   //変数$i = 10; を用意し、10から1まで1つずつカウントダウンするプログラムを作成せよ。(1つずつ改行して表示せよ)
@@ -87,39 +109,56 @@ while($i>=1){
   //以下の配列を表示せよ。(1つずつ改行して表示せよ)
   //この問題にはforeachを用いること
   $food = array('carry', 'onigiri', 'candy', 'ham', 'meet', 'rice');
-
+foreach($food as $value){
+  echo $value."<br>";
+}
+echo "<br>";
   //練習問題11
   //以下の変数を用いて加減乗除のそれぞれの関数を作成せよ。
   $a = 10;
   $b = 5;
-
+function add($a,$b){
+  return $a+$b;
+}
   //課題1
   //西暦の計算
   //以下の配列に2つの西暦a, bがあります。その2つのaからbが何年間か出力しなさい。
   $year = array(1994, 2017);
-
+  echo $year[1]-$year[0];
+  echo "<br>";
   //課題2
   //かまくらづくり
   //櫻井がはしゃいでかまくらを作り始めました。どれだけの体積の雪が必要か計算しなさい。
   //そのかまくらは四角形であり、以下の配列に外側の1辺と内側の1辺の長さが入っている。(r1 > r2)
   //下の行のecho文のコメントを外すとimageが出てくるのでこちらも参照するとわかりやすい。
-  //echo '<img src="./assets/img/img1.png">';
+  echo '<img src="./assets/img/img1.png">';
   $size = array(6, 4);
+  $r1 = $size[0]*$size[0]*$size[0];
+  $r2 = $size[1]*$size[1]*$size[1];
+  echo $r1 - $r2;
 
+  echo "<br>";
   //課題3
   //行列の計算
   //まず、以下のecho文のコメントをはずしてください。
   //以下の配列内にa = 10, b = -20, c = 12, d = 6がそれぞれ代入されている。
   //この行列を表す配列を用いて、行列の計算を行いなさい。
-  //echo '<img src="./assets/img/matrix1.png">';
+  echo '<img src="./assets/img/matrix1.png">';
   $matrix = array(10, -20, 12, 6);
+  $ans = $matrix[0]*$matrix[3] - $matrix[1]*$matrix[2];
+  echo $ans;
 
+  echo "<br>";
   //課題4
   //自己紹介
   //練習問題5を参考にして、自己紹介をせよ。
   //例) 私は、安久昌和です。出身は石川県で、年齢は22歳、趣味はバスケットボールです。電子・情報工学科に所属しています。
   //必ず、連想配列を用いること
+$kadai4 = array('name' => '谷川桂太','born' => '新潟県','age'=>'19','hobby'=>'テニス','major'=>'電子・情報工学科');
+echo '私は、'.$kadai4['name'].'です。'.'出身は'.$kadai4['born'].'で、年齢は'.$kadai4['age'].
+     '、趣味は'.$kadai4['hobby'].'です。'.$kadai4['major'].'に所属しています。';
 
+echo "<br>";
   //課題5
   //1~100までの数字を表示するプログラムを作成せよ。
   //※出力のレイアウトは下記のように表示すること(数字を10個表示したタイミングで改行する)
@@ -131,7 +170,13 @@ while($i>=1){
   ...
   91 92 93 94 95 96 97 98 99 100
   */
-
+  for($i=1;$i<=100;$i++){
+    echo $i.' ';
+    if($i %10 == 0){
+      echo '<br>';
+    }
+  }
+echo "<br>";
   //課題6
   //時差の問題
   //以下の配列に都市の個数、各都市の時差、ある都市の現在時刻が入っている。
@@ -143,10 +188,11 @@ while($i>=1){
 
   //課題7
   //練習問題1~10と課題1~5を関数化せよ。
-
   //課題7.1
   //練習問題1の関数化
-
+  function name($first_name,$last_name){
+    return $first_name.$last_name;
+  }
   //課題7.2
   //練習問題2の関数化
 
